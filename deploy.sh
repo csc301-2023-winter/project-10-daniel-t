aa403562a622931db1a52ceff6564c34d7815612
Vim0315 <dupeng000413@gmail.com>
2023-04-01 01:57:41 -0400

    added a beta version deployment script

---
+++ deploy.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+# Change directory to 'backend' and install requirements
+cd backend || exit
+pip install -r requirements.txt
+
+# Start the Flask app using nohup
+nohup python3 app.py &
+
+# Change back to the home directory
+cd ..
+
+# Change directory to 'my-app' and install dependencies
+cd my-app || exit
+npm install
+
+# Build the React app
+npm run build
+
+# Navigate to the 'build' directory
+cd build || exit
+
+# configure the build folder
+mkdir search
+mv * search
+npx serve &
+nohup "$0" &
