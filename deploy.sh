0177ba60495894ad289302af0e7acf1d2f8adc9d
Vim0315 <dupeng000413@gmail.com>
2023-04-06 16:34:14 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,15 @@
 
 # Change directory to 'backend' and install requirements
 cd backend || exit
-pip install -r requirements.txt
-
+# pip install -r requirements.txt
+# Do the logo caching and create dictionaries
+python3 ./search_logic/Import_Abstract.py
 # Start the Flask app using nohup
 nohup python3 app.py &
 
 # Change back to the home directory
 cd ..
-
+cd frontend
 # Change directory to 'my-app' and install dependencies
 cd my-app || exit
 npm install
